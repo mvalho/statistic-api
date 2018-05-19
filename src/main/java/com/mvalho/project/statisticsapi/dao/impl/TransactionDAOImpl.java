df9d786eda2854de020cac77cefc478a0c3b120a
Marcelo Carvalho <mvalho@gmail.com>
2018-05-19 12:29:26 -0300

    Refactored TransactionDAOImpl added @Component, also refactored TransactionDAOTest to Autowire the component instead of manually created it

--- src/main/java/com/mvalho/project/statisticsapi/dao/impl/TransactionDAOImpl.java
+++ src/main/java/com/mvalho/project/statisticsapi/dao/impl/TransactionDAOImpl.java
@@ -2,10 +2,12 @@ package com.mvalho.project.statisticsapi.dao.impl;
 
 import com.mvalho.project.statisticsapi.dao.TransactionDAO;
 import com.mvalho.project.statisticsapi.entity.Transaction;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class TransactionDAOImpl implements TransactionDAO {
     private List<Transaction> transactionList = new ArrayList<>();
 
--- src/test/java/com/mvalho/project/statisticsapi/TransactionDAOTest.java
+++ src/test/java/com/mvalho/project/statisticsapi/TransactionDAOTest.java
@@ -1,9 +1,12 @@
 package com.mvalho.project.statisticsapi;
 
 import com.mvalho.project.statisticsapi.dao.TransactionDAO;
-import com.mvalho.project.statisticsapi.dao.impl.TransactionDAOImpl;
 import com.mvalho.project.statisticsapi.entity.Transaction;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.math.BigDecimal;
 import java.time.Instant;
@@ -12,12 +15,14 @@ import java.time.ZoneId;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@SpringBootTest
+@RunWith(SpringRunner.class)
 public class TransactionDAOTest {
+    @Autowired
     private TransactionDAO transactionDAO;
 
     @Test
     public void shouldStoreTransactionWhenNewTransactionIsUsed() {
-        this.transactionDAO = new TransactionDAOImpl();
         Transaction transaction = new Transaction(new BigDecimal(12.3), LocalDateTime.ofInstant(Instant.ofEpochSecond(1478192204000L), ZoneId.systemDefault()));
         Transaction expected = new Transaction(new BigDecimal(12.3), LocalDateTime.ofInstant(Instant.ofEpochSecond(1478192204000L), ZoneId.systemDefault()));
 
