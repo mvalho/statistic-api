fe3299a456ffcbcedf7d8109c68d57cd6aecd98a
Marcelo Carvalho <mvalho@gmail.com>
2018-05-20 21:45:22 -0300

    Configuring the SpringBoot application to run and read the components inside the project

--- src/main/java/com/mvalho/project/statisticsapi/StatisticsApiApplication.java
+++ src/main/java/com/mvalho/project/statisticsapi/StatisticsApiApplication.java
@@ -2,8 +2,10 @@ package com.mvalho.project.statisticsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.mvalho.project.statisticsapi"})
 public class StatisticsApiApplication {
 
     public static void main(String[] args) {
